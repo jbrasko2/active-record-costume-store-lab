cec78972e3ea9434d87c693d2784eac58dca2bb1
jbrasko <jbrasko0508@gmail.com>
2020-11-21 17:39:53 -0600

    commit tree 3f31609cf00a868c7546fedd80c972689b3ed45d

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,16 @@
 # Create your costume_stores migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+    def change
+        create_table :costume_stores do |column|
+            column.string :name
+            column.string :location
+            column.integer :costume_inventory
+            column.integer :num_of_employees
+            column.boolean :still_in_business
+            column.datetime :opening_time
+            column.datetime :closing_time
+        end
+    end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,17 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+    def change
+        create_table :haunted_houses do |column|
+            column.string :name
+            column.string :location
+            column.string :theme
+            column.float :price
+            column.boolean :family_friendly
+            column.datetime :opening_date
+            column.datetime :closing_date
+            column.text :description
+        end
+    end
+end
